992039179708912026a55d29f861ff0441fc4623
zfl4wless <noah.bauditz@live.de>
2022-12-01 13:20:35 +0100

    day_01 complete

---
+++ README.md
@@ -0,0 +1,3 @@
+# Advent of Code 2022
+Hohoho, It's my first time trying the AoC! Hopefully i don't mess things up too much :D 
+> **Enjoy my solutions!**
---
+++ day_01/index.ts
@@ -0,0 +1,29 @@
+import * as fs from "fs";
+
+let caloriesList = [];
+let currentCalories = 0;
+fs.readFileSync("./day_01/input.txt", "utf-8")
+  .split(/\r?\n/)
+  .forEach((line, index) => {
+    if (line == "") {
+      caloriesList.push(currentCalories);
+      currentCalories = 0;
+    } else {
+      currentCalories += parseInt(line);
+    }
+  });
+
+/**
+ * Gets the top x numbers from the calories list.
+ *
+ * @param elves The x number of elves to get the top numbers from.
+ * @returns The top x numbers from the calories list.
+ */
+const getHighestCalories = (elves: number) =>
+  caloriesList.sort((a, b) => b - a).slice(0, elves);
+
+// Part 1 //
+console.log(getHighestCalories(1)[0]);
+
+// Part 2 //
+console.log(getHighestCalories(3).reduce((a, b) => a + b));
