898ad7246e9177637dd35efbb341e620150e6e5a
zfl4wless <noah.bauditz@live.de>
2022-12-03 12:50:44 +0100

    day_02 task 2 complete

--- day_02/index.ts
+++ day_02/index.ts
@@ -18,31 +18,57 @@ const choices = {
 };
 
 let score = 0;
+let score_2 = 0;
 fs.readFileSync("./day_02/input.txt", "utf-8")
   .split(/\r?\n/)
   .forEach((line) => {
-    const [enemy, me] = line.split(" ");
+    const [col1, col2] = line.split(" ");
 
-    if (choices[enemy] === choices[me]) {
-      score += Points.TIE;
-    } else if (choices[enemy] == Points.ROCK && choices[me] == Points.PAPER) {
-      score += Points.WIN;
-    } else if (
-      choices[enemy] == Points.PAPER &&
-      choices[me] == Points.SCISSORS
-    ) {
-      score += Points.WIN;
-    } else if (
-      choices[enemy] == Points.SCISSORS &&
-      choices[me] == Points.ROCK
-    ) {
-      score += Points.WIN;
-    } else {
-      score += Points.LOSS;
-    }
-
-    score += choices[me];
+    part_1(col1, col2);
+    part_2(col1, col2);
   });
 
 // Part 1
+function part_1(enemy: string, me: string) {
+  if (choices[enemy] === choices[me]) {
+    score += Points.TIE;
+  } else if (choices[enemy] == Points.ROCK && choices[me] == Points.PAPER) {
+    score += Points.WIN;
+  } else if (choices[enemy] == Points.PAPER && choices[me] == Points.SCISSORS) {
+    score += Points.WIN;
+  } else if (choices[enemy] == Points.SCISSORS && choices[me] == Points.ROCK) {
+    score += Points.WIN;
+  } else {
+    score += Points.LOSS;
+  }
+
+  score += choices[me];
+}
 console.log(score);
+
+// Part 2
+function part_2(enemy: string, result: string) {
+  if (choices[enemy] === Points.ROCK && result === "Z") {
+    score_2 += Points.PAPER;
+    score_2 += Points.WIN;
+  } else if (choices[enemy] === Points.PAPER && result === "Z") {
+    score_2 += Points.SCISSORS;
+    score_2 += Points.WIN;
+  } else if (choices[enemy] === Points.SCISSORS && result === "Z") {
+    score_2 += Points.ROCK;
+    score_2 += Points.WIN;
+  } else if (choices[enemy] === Points.ROCK && result === "X") {
+    score_2 += Points.SCISSORS;
+    score_2 += Points.LOSS;
+  } else if (choices[enemy] === Points.PAPER && result === "X") {
+    score_2 += Points.ROCK;
+    score_2 += Points.LOSS;
+  } else if (choices[enemy] === Points.SCISSORS && result === "X") {
+    score_2 += Points.PAPER;
+    score_2 += Points.LOSS;
+  } else {
+    score_2 += choices[enemy];
+    score_2 += Points.TIE;
+  }
+}
+console.log(score_2);
