752dd1f4d92ad94f0178cf153c2335d5a255921c
zfl4wless <noah.bauditz@live.de>
2022-12-07 13:05:32 +0100

    day_03 task 2 complete

--- day_03/AdventDay03.ts
+++ day_03/AdventDay03.ts
@@ -1,24 +1,35 @@
 import { AdventDay } from "../core/AdventDay.ts";
 
 export class AdventDay03 extends AdventDay {
-  private prioritySumItemTypes: number;
+  private prioritySumItemTypesCompartments: number;
+  private prioritySumItemTypesElfGroups: number;
+  private badges: string[];
+  private iterationCount: number;
 
   constructor() {
     super(3);
 
-    this.prioritySumItemTypes = 0;
+    this.prioritySumItemTypesCompartments = 0;
+    this.prioritySumItemTypesElfGroups = 0;
+    this.badges = [];
+    this.iterationCount = 1;
     this.input.forEach((line) => {
-      this.calculatePrioritySumItemTypes(line);
+      this.calculatePrioritySumItemTypesFromCompartments(line);
+      this.calculatePrioritySumItemTypesFromElfGroups(line);
+
+      this.iterationCount++;
     });
   }
 
   part01(): void {
-    console.log(this.prioritySumItemTypes);
+    console.log(this.prioritySumItemTypesCompartments);
   }
 
-  part02(): void {}
+  part02(): void {
+    console.log(this.prioritySumItemTypesElfGroups);
+  }
 
-  private calculatePrioritySumItemTypes(line: string) {
+  private calculatePrioritySumItemTypesFromCompartments(line: string) {
     const [firstCompartment, secondCompartment] = [
       line.substring(0, line.length / 2),
       line.substring(line.length / 2),
@@ -36,7 +47,32 @@ export class AdventDay03 extends AdventDay {
         ? alphabet.indexOf(sharedCharacter) + 1
         : alphabet.indexOf(sharedCharacter.toLowerCase()) + 27;
 
-    this.prioritySumItemTypes += priority;
+    this.prioritySumItemTypesCompartments += priority;
+  }
+
+  private calculatePrioritySumItemTypesFromElfGroups(line: string) {
+    this.badges.push(line);
+
+    if (this.iterationCount % 3 === 0) {
+      const [firstBadge, secondBadge, thirdBadge] = this.badges;
+
+      const sharedCharacter = firstBadge
+        .split("")
+        .find(
+          (char) => secondBadge.includes(char) && thirdBadge.includes(char)
+        );
+      if (!sharedCharacter) {
+        return;
+      }
+
+      const priority =
+        sharedCharacter === sharedCharacter.toLowerCase()
+          ? alphabet.indexOf(sharedCharacter) + 1
+          : alphabet.indexOf(sharedCharacter.toLowerCase()) + 27;
+
+      this.prioritySumItemTypesElfGroups += priority;
+      this.badges = [];
+    }
   }
 }
 
