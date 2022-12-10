6ab5143da16f1229ac36859238fd70a705a25bd7
zfl4wless <noah.bauditz@live.de>
2022-12-10 12:27:19 +0100

    day_10 task 2 complete

--- day_10/AdventDay10.ts
+++ day_10/AdventDay10.ts
@@ -4,6 +4,7 @@ export class AdventDay10 extends AdventDay {
     private register: number;
     private currentCycle: number;
     private signalStrengthSum: number;
+    private sprite: string[][];
 
     constructor() {
         super(10);
@@ -11,6 +12,7 @@ export class AdventDay10 extends AdventDay {
         this.register = 1;
         this.currentCycle = 0;
         this.signalStrengthSum = 0;
+        this.sprite = [[], [], [], [], [], []];
         this.input.forEach((line) => {
             this.parseInput(line);
         });
@@ -20,15 +22,21 @@ export class AdventDay10 extends AdventDay {
         console.log(this.signalStrengthSum);
     }
 
-    part02(): void {}
+    part02(): void {
+        this.sprite.forEach((row) => {
+            console.log(row.join(''));
+        });
+    }
 
     private parseInput(line: string): void {
+        this.drawSprite();
         this.currentCycle++;
         this.increaseSignalStrengthSum();
 
         if (line.startsWith('addx')) {
             const [_cmd, value] = line.split(' ');
 
+            this.drawSprite();
             this.currentCycle++;
             this.increaseSignalStrengthSum();
             this.register += parseInt(value);
@@ -38,6 +46,21 @@ export class AdventDay10 extends AdventDay {
     private increaseSignalStrengthSum(): void {
         if (this.currentCycle === 20 || (this.currentCycle + 20) % 40 === 0) {
             this.signalStrengthSum += this.currentCycle * this.register;
+        }
+    }
+
+    private currentSpriteRow = 0;
+    private drawSprite(): void {
+        const spriteCol = this.currentCycle % 40;
+
+        if (this.register === spriteCol || this.register + 1 === spriteCol || this.register - 1 === spriteCol) {
+            this.sprite[this.currentSpriteRow].push('#');
+        } else {
+            this.sprite[this.currentSpriteRow].push('.');
+        }
+
+        if ((this.currentCycle + 1) % 40 === 0) {
+            this.currentSpriteRow++;
         }
     }
 }
